a2320b0c7dd3e5f301c6eaf73744510eb94af0ce
acer <guliyevkanann@gmail.com>
2024-06-07 00:46:49 +0400

    For Loop

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        int[] numbers = new int[]{1, 3, 5, 7, 9};
-        int searchNumber = 1;
-        boolean found = false;
-
-        for (int i = 0; i < numbers.length; i++) {
-            if (numbers[i] == searchNumber) {
-                found = true;
-                break;
-            }
-        }
-        if (found) {
-            System.out.println("Yes it is Here!");
-        } else {
-            System.out.println("Not it is Here!");
-        }
+     int number=2;
+     for(int i=1;i<10;i++){
+         System.out.println(number+"x" +i + "="+number*i);
+     }
+     }
     }
-}
+
